4465bcfeadcf4ee6ce5feb8d645741683fd8088d
Oliver Suchanek <oliversuchanek@hotmail.co.uk>
2017-11-02 20:46:59 +0000

    problems solved

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(num1, num2)
+	return num1 + num2
+end
+
+def subtract(num1, num2)
+	return num1 - num2
+end
+
+def sum arr
+  sum = 0;
+  arr.length.times do |i|
+    sum += arr[i]
+  end
+  return sum
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
 #write your code here
+def echo string
+	return string
+end
+
+def shout string
+	return string.upcase
+end
+
+def repeat(string, count=2)
+	return [string] * count * ' '
+end
+
+def start_of_word(string, count=0)
+	return string[0,count]
+end
+
+def first_word(string)
+	return string.split.first
+end
+
+def titleize string
+  little_words = %w[and the over]
+  array = string.split(" ")
+  array = [array[0].capitalize!] + array[1..-1].each do |word|
+    unless little_words.include? word
+      word.capitalize!
+    end
+  end
+  array.join(" ")
+end
