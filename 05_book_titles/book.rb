f62461b5cc14683969a91ece58b0a9062f80b7be
Oliver Suchanek <oliversuchanek@hotmail.co.uk>
2017-11-03 19:44:40 +0000

    Completion

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,11 @@
 class Book
-# write your code here
+
+attr_accessor :title
+
+def title=(bookname)
+a=bookname.split.each{|i|i.capitalize! if i != "and" && i != "in" && i != "the" && i != "an" && i != "of" && i != "a"}
+a[0].capitalize!
+@title=a.join(" ")
+end
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,11 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds  #attr_accessor creates instance variable @seconds
+
+  def initialize
+  	@seconds = 0
+  end
+
+  def time_string
+  	Time.at(@seconds).utc.strftime("%H:%M:%S")
+  end
 end
